bb9cdee472efdc7ff32a34de1ab25fffffd050d7
angelasmilford <angela.milford123@gmail.com>
2025-09-23 10:43:47 -1100

    Created a method that calculates sales totals

--- Program.cs
+++ Program.cs
@@ -29,4 +29,24 @@ IEnumerable<string> FindFiles(string folderName)
     return salesFiles;
 }
 
-record SalesData (double Total);
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {      
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData(double Total);
