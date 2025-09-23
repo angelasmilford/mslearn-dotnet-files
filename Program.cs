ba7b1c7e1236319ab3bbeb5f1a2ed584f490afbf
angelasmilford <angela.milford123@gmail.com>
2025-09-23 10:49:55 -1100

    Ran the program

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿using System.IO;
 using System.Collections.Generic;
-using Newtonsoft.Json;
+using Newtonsoft.Json; 
 
 var currentDirectory = Directory.GetCurrentDirectory();
-var storesDir = Path.Combine(currentDirectory, "stores");
+var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);
+Directory.CreateDirectory(salesTotalDir);   
 
-var salesFiles = FindFiles(storesDir);
+var salesFiles = FindFiles(storesDirectory);
 
 var salesTotal = CalculateSalesTotal(salesFiles);
 
@@ -35,21 +35,21 @@ IEnumerable<string> FindFiles(string folderName)
 double CalculateSalesTotal(IEnumerable<string> salesFiles)
 {
     double salesTotal = 0;
-
+    
     // Loop over each file path in salesFiles
     foreach (var file in salesFiles)
-    {
+    {      
         // Read the contents of the file
         string salesJson = File.ReadAllText(file);
-
+    
         // Parse the contents as JSON
         SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
-
+    
         // Add the amount found in the Total field to the salesTotal variable
         salesTotal += data?.Total ?? 0;
     }
-
+    
     return salesTotal;
 }
 
-record SalesData(double Total);
+record SalesData (double Total);
